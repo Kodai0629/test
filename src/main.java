3480e4511dd6a092143c676dd621abdb280c74a5
okada <cist.b218.k.okada@gmail.com>
2019-09-10 19:52:32 +0900

    現状

--- src/main.java
+++ src/main.java
@@ -1,25 +1,34 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class main {
     public static void main(String[] args) {
-            try {
-                Class.forName("org.h2.Driver");
-            }catch (ClassNotFoundException e){
-                e.printStackTrace();
-            }try (Connection connection = DriverManager.getConnection("jdbc:h2:~/h2db/lecture;Mode=PostgreSQL;　AUTO_SERVER=TRUE;", "Okada", "kodai0629");
-                  Statement statement=connection.createStatement()){
-                String sql="insert into student(name,student_number,department)" +
-                        "values ('山田　太一郎',1,'一般')";
-                statement.executeUpdate(sql);
-            } catch(SQLException e) {
-                e.printStackTrace();
+        String sql = "insert into student(name, department) VALUES (?, ?)";
 
-            }
-
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
 
-    }
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:h2:~/h2db/lecture;Mode=PostgreSQL;AUTO_SERVER=TRUE;",
+                    "b2180520", "kodai0629");
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "Kodai Okadai");
+            preparedStatement.setString(2, "情報システム工学科");
+            preparedStatement.execute();
+            sql ="insert into GRADE(student_name, point) values (?,?)";
 
+            PreparedStatement preparedstatement = connection.prepareStatement(sql);
+            preparedstatement.setString(1, "Kodai Okadai");
+            preparedstatement.setString(2, "59");
+            preparedstatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
